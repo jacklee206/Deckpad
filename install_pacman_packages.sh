ecdfbf4f1f37b8a3121b077aeddb2caf2d4b30f6
pandus <ggmm206@gmail.com>
2023-06-04 17:47:04 +0800

    Update install_pacman_packages.sh

--- install_pacman_packages.sh
+++ install_pacman_packages.sh
@@ -4,7 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 
+# Purge old config file
+sudo rm -f /etc/vpnc/vpnc-script
+sudo rm -f /etc/skel/.screenrc
+
 sudo pacman -S --noconfirm xorg-xinput
 sudo pacman -S --noconfirm figlet
+sudo pacman -S --noconfirm --needed networkmanager-openconnect screen tmux
 
-cd - >/dev/null
+
+cd - >/dev/null 
