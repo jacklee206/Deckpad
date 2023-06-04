50d059317ab687c22eb87a11982526106c76bc21
pandus <ggmm206@gmail.com>
2023-06-04 17:53:50 +0800

    Update install_pacman_packages.sh

--- install_pacman_packages.sh
+++ install_pacman_packages.sh
@@ -4,9 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 
-# Purge old config file
-sudo rm -f /etc/vpnc/vpnc-script
-sudo rm -f /etc/skel/.screenrc
 
 sudo pacman -S --noconfirm xorg-xinput
 sudo pacman -S --noconfirm figlet
